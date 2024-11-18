400fd3d4d238692db29910e55457fbd2944acd58
Jiahao <yaojiahao048@gmail.com>
2024-11-18 20:12:21 +0800

    testing import def from other file

--- Code_1.py
+++ Code_1.py
@@ -1,12 +1,13 @@
+import ai as b
 print('hello')
 print ("poo.py")
 print('abc')
 print('heloooooo')
+b.a()
 if 1==1:
     print ("poo.py")
     print('abc')
     print("testing")
-
     print ("Ryan was here to test it out!")
     print('bye')
-    
+    b.a()
