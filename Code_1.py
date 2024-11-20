c5eda6583af3be78fc9d9122267fb65184be379d
z <zczconly@gmail.com>
2024-11-20 13:29:24 +0800

    aaa

--- Code_1.py
+++ Code_1.py
@@ -5,10 +5,18 @@ print('abc')
 print('heloooooo')
 ai.a()
 if 1==1:
-    print ("poo.py")
+    print("poo.py")
     print('abc')
     print("testing")
-    print ("Ryan was here to test it out!")
+    print("Ryan was here to test it out!")
     print('bye')
     ai.a()
 print('b')
+
+
+def start():
+    user_enters = input('Welcome to Porotimer! What is your name?') 
+    print ('welcome', user_enters)
+
+
+start()
