c533998904111587cabae2cffb0c93f3b1710c77
howeubean <ryanngoeugene@gmail.com>
2024-11-26 11:04:18 +0800

    eu dictionary pseudocoding

--- py/eu test/dictionary.py
+++ py/eu test/dictionary.py
@@ -2,3 +2,37 @@ import timer as t
 t.timer(5 , 5)
 
 # def dictionary():
+
+"""
+create session: open code, make new list within dictionary
+this is to happen every time the start button is pressed
+naming to be the current length of dictionary + 1
+"""
+# making new list within dictionary 
+"""Example dictionary"""
+#my_dict = {"key1": [1, 2, 3], "key2": [4, 5, 6]}
+"""Adding a new key with an empty list"""
+#my_dict["key3"] = []
+"""Printing the updated dictionary"""
+#print(my_dict)
+
+
+'''
+storing: adding to the new empty list happens every time the 
+1) loop ends
+2) they quit after pausing
+'''
+# use append method for this part
+
+'''
+naming: the saved list/value under a specified key
+ask for input name/label for study session
+ask for confirmation, 
+if "no", go back to asking for input
+if "yes", save key name for that list
+'''
+
+
+'''
+session_time: function to sum the total of a specified session
+'''
