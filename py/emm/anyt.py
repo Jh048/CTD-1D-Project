5783db42420a7f793390b2fae61e3511af95b6a7
jen <wongmeilingjennifer@gmail.com>
2024-11-28 17:23:50 +0800

    stuff

---
+++ py/emm/anyt.py
@@ -0,0 +1,27 @@
+import sys
+import os
+
+# Add the parent folder (CTD 1D CODE) to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
+# Import the timer function from Timer.py
+from timer import timer  # Match the exact name of Timer.py (case-sensitive)
+
+
+
+
+
+Start_or_archieve = input('Do you want to start a timer?(Y/N)'). upper ()
+if Start_or_archieve == 'Y':
+    Title = input ("YES was entered. What are we doing today?")
+    Duration = input (f" How long do you want to {Title} for?")
+    #timer function
+    timer()
+else:
+    Archieve= input ('Do you want to look at your history?(Y/N)') .upper()
+    if Archieve == 'Y':
+        print ('Loading History..........')
+        #Histroy function
+    else:
+        print ('Bye! Have a great day!')
+        #go back main (main function)
