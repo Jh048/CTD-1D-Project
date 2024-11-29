a48e3eb9a84f9799e2fe441764fa40b22bc57a39
howeubean <ryanngoeugene@gmail.com>
2024-11-29 13:57:50 +0800

    digits to format definition

--- py/eu test/testA.py
+++ py/eu test/testA.py
@@ -5,8 +5,19 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 # Import the timer function from Timer.py
-from timer import timer, timer_data, elapsed_time_str # Match the exact name of Timer.py (case-sensitive)
+from timer import timer, timer_data  # Match the exact name of Timer.py (case-sensitive)
+archive_dict = {}
+# timer(10)
+# print(timer_data)
 
-timer()
-print(timer_data)
-print(elapsed_time_str)
+# def store():
+#     import elapsed_time  
+#     archive_dict[Title].append('')
+
+def secs_to_clock(s):
+    mins, secs = divmod(s, 60)
+    hours, mins = divmod(mins, 60)
+    return f"{hours:02}:{mins:02}:{secs:02}" if hours > 0 else f"{mins:02}:{secs:02}"
+
+
+print(secs_to_clock(108576))
