5094baf6a5973a3d7a063da124af8e6faf7d0d38
howeubean <ryanngoeugene@gmail.com>
2024-11-29 14:27:49 +0800

    trials

--- py/eu test/testA.py
+++ py/eu test/testA.py
@@ -4,20 +4,32 @@ import os
 # Add the parent folder (CTD 1D CODE) to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
-# Import the timer function from Timer.py
 from timer import timer, timer_data  # Match the exact name of Timer.py (case-sensitive)
 archive_dict = {}
 # timer(10)
 # print(timer_data)
+# Import the timer function from Timer.py
+def start_or_archive():
+    Start_or_archieve = input('Do you want to start a timer?(Y/N)'). upper ()
+    if Start_or_archieve == 'Y':
+        Title = input ("YES was entered. What are we doing today?")
+        timer()
+    else:
+        Archieve= input ('Do you want to look at your history?(Y/N)') .upper()
+        if Archieve == 'Y':
+            print ('Loading History..........')
+            #Histroy function
+        else:
+            print ('Bye! Have a great day!')
+            #go back main (main function)
 
-# def store():
-#     import elapsed_time  
-#     archive_dict[Title].append('')
+def store():
+    import elapsed_time  
+archive_dict[Title].append('')
 
-def secs_to_clock(s):
-    mins, secs = divmod(s, 60)
+def secs_to_clock(sec):
+    mins, secs = divmod(sec, 60)
     hours, mins = divmod(mins, 60)
     return f"{hours:02}:{mins:02}:{secs:02}" if hours > 0 else f"{mins:02}:{secs:02}"
 
 
-print(secs_to_clock(108576))
