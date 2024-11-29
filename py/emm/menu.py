b037a585e7d98506229faea13b5441b94ad55217
jen <wongmeilingjennifer@gmail.com>
2024-11-29 15:19:18 +0800

    menu

---
+++ py/emm/menu.py
@@ -0,0 +1,37 @@
+menu1 ="""
+--------------
+Do you want to start a timer?
+(Y)Yes
+(N)No
+----------------------------
+"""
+
+menu2 ="""
+--------------
+Do you want to start a timer?
+(Y)Yes
+(N)No
+----------------------------
+"""
+
+menu3 = """                     #yes was entered at menu2
+--------------------------
+What are we doing today?
+eg. Study, Work etc
+--------------------------
+"""
+menu5 = """
+------------------------------------
+Do you want to look at your history?
+(Y)) Yes
+(N)) No
+------------------------------------
+"""
+menu6 ="""
+------------------------------------
+Enter countdown time in HH,MM,SS format:
+eg. 01,20,30  (1hr 20min 30 secs)
+(N)) No
+------------------------------------
+"""
+
