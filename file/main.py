a789722bbe390d8a4e7295083e5795024a1ac79f
howeubean <ryanngoeugene@gmail.com>
2024-12-06 17:18:11 +0800

    .

--- file/main.py
+++ file/main.py
@@ -54,7 +54,7 @@ def history():
             display_time_summary(total)
             return
         elif sel == "2":
-            end()
+            exit_option()
         else:
             retry(history)
 
